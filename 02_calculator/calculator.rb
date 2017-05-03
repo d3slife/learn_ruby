c576fa8ee3c9bfaf06c1c4f02a24f825a7d766d0
laplas <d3slife@gmail.com>
2017-05-03 17:42:19 +0300

    Added code to calculator_spec's file with new test cases and appropriate code to calculator file

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+# Addition
+def add first_operand, second_operand
+  first_operand + second_operand
+end
+
+# Subtraction
+def subtract first_operand, second_operand
+  first_operand - second_operand
+end
+
+# Summation
+def sum array
+  total = 0
+  array.each do |number|
+    total += number
+  end
+  total
+end
+
+# Multiplication
+def multiply array
+  total = 1
+  array.each do |number|
+    total *= number
+  end
+  total
+end
+
+# Power of
+def power first_operand, second_operand
+  first_operand ** second_operand
+end
+
+# Factorial
+def factorial number
+  if(number <= 1)
+    1
+  else
+    factorial(number - 1) * number
+  end
+end
