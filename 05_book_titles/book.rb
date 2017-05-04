8035dad93e56672d08d6cb3b6ab3ed18401d1dac
laplas <d3slife@gmail.com>
2017-05-04 11:42:19 +0300

    Added code to pass rspec specification requirements

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title= book_name
+    conjunctions = ["for", "and", "nor", "but", "or", "yet", "so"]
+    prepositions = ["on", "in", "at", "since", "for", "ago", "before", "to", "past", "to", "till", "until", "by", "of"]
+    artiles = ["a", "an", "the"]
+    reply = book_name.split
+    reply.each_with_index do |word, index|
+      if index == 0
+        reply[index] = word.capitalize
+      else
+        if (not conjunctions.include?(word)) && (not prepositions.include?(word)) && (not artiles.include?(word))
+            reply[index] = word.capitalize
+        end
+      end
+    end
+    @title = reply.join(" ")
+  end
 end
