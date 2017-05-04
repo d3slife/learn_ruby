138c1751cb3bc8200fc5f0912a4cfb26ed1a16b6
laplas <d3slife@gmail.com>
2017-05-04 11:07:30 +0300

    Added code to successfully pass the 04_pig_latin's rspec specification requirements

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
 #write your code here
+def translate question
+  reply = question.split
+  reply.each_with_index do |phrase, position|
+    reply[position] = pig_latin(phrase)
+  end
+  reply.join(" ")
+end
+
+def pig_latin phrase
+  vowels = ["a", "e", "i", "o", "u"]
+  if vowels.include?(phrase[0])
+    phrase + "ay"
+  else
+    index = 0
+    while not vowels.include?(phrase[index])
+      index += 1
+    end
+    # Treating "qu" as a single phoneme
+    if phrase[index-1, 2] == "qu"
+      phrase[index+1..-1] + phrase[0..index] + "ay"
+    else
+      phrase[index..-1] + phrase[0..index-1] + "ay"
+    end
+  end
+end
