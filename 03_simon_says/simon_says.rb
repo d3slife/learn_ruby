3ab669f05962f2b0b80a7bb21e7af2c01c1fdeff
laplas <d3slife@gmail.com>
2017-05-04 09:47:09 +0300

    Major changes to simon_says.rb. Each specification case is implemented.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,6 +6,33 @@ def shout phrase
   phrase.upcase
 end
 
-def repeat phrase
-  "#{phrase} #{phrase}"
+def repeat phrase, repeat = 2
+  reply = []
+  repeat.times do
+    reply.push(phrase)
+  end
+  reply.join(" ")
+end
+
+def start_of_word phrase, number_of_letters
+  phrase[0, number_of_letters]
+end
+
+def first_word phrase
+  phrase.split[0]
+end
+
+def titleize phrase
+  reply = phrase.split
+  reply.each_with_index do |word, i|
+    if i == 0
+      reply[i] = word.capitalize
+    else
+      if (word.length > 3) && (word != "over")
+        reply[i] = word.capitalize
+      end
+    end
+
+  end
+  reply.join(" ")
 end
