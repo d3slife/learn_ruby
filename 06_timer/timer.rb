29f3dbd143f3ae71029ff1b14aca99dce6baba23
laplas <d3slife@gmail.com>
2017-05-04 12:07:19 +0300

    Added code to pass rspec specification requirements

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    seconds_formatted = @seconds % 60
+    minutes_formatted = @seconds / 60 % 60
+    hours_formatted = @seconds / 60 / 60 % 24
+    sprintf("%02d:%02d:%02d", hours_formatted, minutes_formatted, seconds_formatted)
+  end
 end
